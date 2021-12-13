5c88c5c25fff96172c81ef9d7596cad4f7074ac9
vilinh <vilinhlvu@gmail.com>
2021-12-13 11:07:06 -0800

    working on responsive navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/Navbar.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+
+    const [showLinks, setShowLinks] = useState(false);
+
     return (
         <nav>
             <div className='logo'>
                 <Link to ="/"><h4>plant recipes</h4></Link>
             </div>
-            <ul className='nav-links'>
+            <ul className='nav-links' id={showLinks ? "hidden" : ""}>
                 <Link to ="/"><li>Home</li></Link>
                 <Link to ="/recipes"><li>Recipes</li></Link>
                 <Link to ="/about"><li>About</li></Link>
                 <li>Contact</li>
             </ul>
+            <div class="menu-btn"  id={showLinks ? "showmenu" : ""}>
+                <i onClick={() => setShowLinks(!showLinks)}class="fa fa-bars"></i>
+            </div>
         </nav>
     )
 }
