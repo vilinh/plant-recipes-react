bb0fae37e9b3d14e719075150b540594ef14f953
vilinh <vilinhlvu@gmail.com>
2021-12-13 14:58:18 -0800

    media queries for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,8 +14,8 @@ export default function Home() {
                         <p>Learn how to incorporate more whole foods and vegetables
                         into your diet through simple recipes.</p>
                         <div className='buttons'>
-                        <Link to="/recipes"><div className='b1'>Explore Recipes</div></Link>
-                        <Link to="/about"><div className='b2'>About</div></Link>
+                            <Link to="/recipes"><div className='b1'>Explore Recipes</div></Link>
+                            <Link to="/about"><div className='b2'>About</div></Link>
                         </div>                
                     </div>
                     <div className="header-right">
