3437276cc706695aaa4d9275cf25fc499d37b61f
vilinh <vilinhlvu@gmail.com>
2021-12-13 16:22:33 -0800

    added contact form

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/Contact.css';
+
+export default function Contact() {
+    return (
+        <div>
+            <div className="contact-form">
+                <h1>Get in Touch</h1>
+                <p>Questions? Concerns? Suggestions? Send them over!</p>
+                <form action="contactform.php" method="post">
+                    <input name="name" id="name" type="text" className="feedback-input" placeholder="Name"/>
+                    <input name="email" id="email" type="email" className="feedback-input" placeholder="E-mail"/>
+                    <textarea name="message" id="message" type="text" className="feedback-input" placeholder="Message"></textarea>
+                    <input onclick="sendContact()" type="submit" value="send"/>
+                </form>
+            </div>
+        </div>
+    )
+}
