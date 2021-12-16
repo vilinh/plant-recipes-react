422f05ef556444f788ca8707bc5efa160a7e9730
vilinh <vilinhlvu@gmail.com>
2021-12-16 10:54:18 -0800

    used state for gallery

--- frontend/src/pages/Gallery.jsx
+++ frontend/src/pages/Gallery.jsx
@@ -1,35 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Gallery.css';
 
 export default function Gallery({data}) {
-    // array of images
+    // array of images & current index
+    const [index, setIndex] = useState(0);
+    const length = data.length;
     var images = []
     var desc = []
     data.map((recipe) => {
         images.push(recipe.image)
         desc.push(recipe.desc)
     })
-    var slider = document.getElementById("slider");
-    var p = document.getElementById("desc");
-    var num = 0;
 
-    function prev() {
-        num--;
-        if(num < 0) {
-            num = images.length-1;
-        }
-        slider.src = images[num]
-        p.innerHTML = desc[num]
-        console.log(num)
+    const next = () => {
+        setIndex(index === length - 1 ? 0 : index + 1);
+        console.log(index)
     }
-    function next() {
-        num++;
-        if (num > images.length-1) {
-            num = 0;
-        }
-        slider.src = images[num]
-        p.innerHTML = desc[num]
-        console.log(num)
+    const prev = () => {
+        setIndex(index === 0 ? length - 1 : index - 1);
+        console.log(index)
     }
 
     return (
@@ -37,12 +26,12 @@ export default function Gallery({data}) {
             <div className="gallery-panel">
                 <div className="text">
                     <h1>On the Blog</h1>
-                    <p id="desc">{desc[num]}</p>
+                    <p id="desc">{desc[index]}</p>
                 </div>
                 <div className="images">
-                    <i class="fas fa-chevron-left sliders"
+                    <i className="fas fa-chevron-left sliders"
                     onClick={prev}></i>
-                    <img id="slider" src="https://i.imgur.com/paXIGwf.png"/>
+                    <img id="slider" src={images[index]}/>
                     <i className="fas fa-chevron-right sliders"
                     onClick={next}></i>
                 </div>
