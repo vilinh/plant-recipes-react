2c0210051ca21107b9e7b17753c0c4697ccdb7ec
vilinh <vilinhlvu@gmail.com>
2022-04-24 21:49:46 -0700

    rid unnecessary package

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require("path");
-const nodemailer = require("nodemailer");
 
 const app = express()
 app.use(express.json())
